58dd22dcea4981e86d09b349de34111d83dba5e0
Roberto Quintero <langdonhp435@gmail.com>
2026-07-25 13:38:27 -0600

    02 security Add security headers to Next.js configuration for enhanced security

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,31 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+];
+
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins:['192.168.1.68']
+  allowedDevOrigins: ["192.168.1.68"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
